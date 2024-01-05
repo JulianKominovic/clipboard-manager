782a2a75f347dc8477c82d25cfff0f0d0c1e169a
Julian Kominovic <juliankominovic@gmail.com>
2024-01-04 21:50:53 -0300

    feat: render text copied

--- src/sections/Main.tsx
+++ src/sections/Main.tsx
@@ -7,6 +7,8 @@ import {
   getClipboardHistory,
 } from "../events";
 import { APP_DATA_DIR } from "../constants";
+import { getRelativeDate } from "../utils";
+import Highlight from "react-highlight";
 
 console.log(APP_DATA_DIR);
 
@@ -22,6 +24,26 @@ export default function Main() {
       textItems: clipboardHistory.filter((item) => item.text),
     };
   }, [clipboardHistory]);
+  const grouppedByRelativeDate: Map<string, ClipboardHistoryItemWithImage[]> =
+    useMemo(() => {
+      const groups = new Map<string, ClipboardHistoryItemWithImage[]>();
+      clipboardHistory
+        .sort(
+          (a, b) =>
+            new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
+        )
+        .forEach((item) => {
+          const relativeDate = getRelativeDate(new Date(item.timestamp));
+          const existingGroup = groups.get(relativeDate);
+          if (existingGroup) {
+            groups.get(relativeDate)?.push(item);
+          } else {
+            groups.set(relativeDate, [item]);
+          }
+        });
+      return groups;
+    }, [textItems]);
+
   useEffect(() => {
     getClipboardHistory().then((history) => {
       setClipboardHistory(history);
@@ -30,7 +52,7 @@ export default function Main() {
   }, []);
 
   return (
-    <div className="w-full px-4">
+    <div className="w-full h-full px-4 overflow-y-auto">
       <RecentItems>
         {imagesItems
           .slice(0, 10)
@@ -45,6 +67,44 @@ export default function Main() {
             );
           })}
       </RecentItems>
+      <div>
+        {[...grouppedByRelativeDate.entries()].map(([key, items]) => {
+          return (
+            <section key={key} className="my-6">
+              <header className="flex items-center gap-4 my-4 text-xs text-neutral-400">
+                <h2>{key}</h2>
+                <hr className="flex-grow border-neutral-300" />
+              </header>
+              <ul className="flex flex-col gap-2">
+                {items.map((item) => {
+                  if (
+                    item.content_type === "Image" &&
+                    item.imageSrc &&
+                    item.image_filename
+                  ) {
+                    return (
+                      <li key={item.id}>
+                        <ImageCard
+                          databaseId={item.id}
+                          src={item.imageSrc as string}
+                        />
+                      </li>
+                    );
+                  }
+                  return (
+                    <li
+                      key={item.id}
+                      className="[&>pre]:p-2 overflow-hidden border rounded-md [&>pre]:w-full [&>pre]:pb-4 [&>pre]:overflow-auto"
+                    >
+                      <Highlight>{item.text}</Highlight>
+                    </li>
+                  );
+                })}
+              </ul>
+            </section>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,27 @@
-import { ClipboardHistoryItem } from "../events";
+const second = 1000;
+const minute = 60 * 1000;
+const hour = minute * 60;
+const day = hour * 24;
+const month = day * 30;
+const year = day * 365;
+export function getRelativeDate(date: Date) {
+  const intl = new Intl.RelativeTimeFormat(undefined, { numeric: "auto" });
+
+  const relativeTime = Date.now() - date.getTime();
+  const elapsed = Math.abs(relativeTime);
+
+  const sign = Math.sign(-relativeTime);
+  if (elapsed < minute) {
+    return intl.format(sign * Math.round(elapsed / second), "second");
+  } else if (elapsed < hour) {
+    return intl.format(sign * Math.round(elapsed / minute), "minute");
+  } else if (elapsed < day) {
+    return intl.format(sign * Math.round(elapsed / hour), "hour");
+  } else if (elapsed < month) {
+    return intl.format(sign * Math.round(elapsed / day), "day");
+  } else if (elapsed < year) {
+    return intl.format(sign * Math.round(elapsed / month), "month");
+  } else {
+    return intl.format(sign * Math.round(elapsed / year), "year");
+  }
+}
